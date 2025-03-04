216241df77692158e6beee33c2cbab4aa1fe21fb
vinayjadaun11112003 <vinayjadaun11112003@gmail.com>
2025-03-04 16:11:49 +0530

    [Vinay Jadaun] - Add. coinFlip

---
+++ If-else_02/coinFlip.js
@@ -0,0 +1,8 @@
+// Simulate a coin flip (0 = Heads, 1 = Tails)
+let flip = Math.floor(Math.random() * 2);
+
+// Determine the result
+let result = (flip === 0) ? "Heads" : "Tails";
+
+// Print the result of the coin flip
+console.log("Coin flip result:", result);
